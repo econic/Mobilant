392c3ef08383dd9bd086169a0974f3697fffe8eb
Aimo Künkel <mail@aimo.cc>
2013-10-09 19:36:32 +0200

    initial upload

---
+++ Mobilant.php
@@ -0,0 +1,317 @@
+<?php
+/**
+ * PHP class for the mobilant.de services
+ * API: http://mobilant.de/
+ * 
+ * @author Aimo Künkel <mail@aimo.cc>
+ * @package mobilant
+ * @license MIT License
+ */ 
+
+class Mobilant {
+	// api url
+	const API_URL = 'https://gw.mobilant.net';
+
+	/**
+	 * Identification Key
+	 * 
+	 * @var string
+	 */
+	protected $key;
+
+	/**
+	 * text of the message
+	 * 
+	 * @var string
+	 */
+	protected $message;
+
+	/**
+	 * phone number of the receiver
+	 * 
+	 * @var string
+	 */
+	protected $to;
+
+	/**
+	 * sender name
+	 * 
+	 * @var string
+	 */
+	protected $from;
+
+	/**
+	 * the route to send the sms with, check available options on mobilant.com
+	 * 
+	 * @var string
+	 */
+	protected $route;
+
+	/**
+	 * reference of the sms, if you wish to add one
+	 * 
+	 * @var string
+	 */
+	protected $ref;
+
+	/**
+	 * concat, tells if messages > 160 chars should be splitted or concatenated
+	 * 
+	 * @var boolean
+	 */
+	protected $concat;
+
+	/**
+	 * senddate if you wish to time the sms
+	 * 
+	 * @var integer
+	 */
+	protected $senddate;
+
+	/**
+	 * send debugging sms
+	 * 
+	 * @var boolean
+	 */
+	protected $debug;
+
+	/**
+	 * Gets the key
+	 *
+	 * @return string
+	 */
+	public function getKey() {
+		return $this->key;
+	}
+	
+	/**
+	 * Sets the key
+	 *
+	 * @param string $key value to set
+	 * @return this
+	 */
+	public function setKey($key) {
+		$this->key = (string)$key;
+		return $this;
+	}
+	
+	/**
+	 * Gets the message
+	 *
+	 * @return string
+	 */
+	public function getMessage() {
+		return $this->message;
+	}
+	
+	/**
+	 * Sets the message
+	 *
+	 * @param string $message value to set
+	 * @return this
+	 */
+	public function setMessage($message) {
+		$this->message = (string)$message;
+		return $this;
+	}
+	
+	/**
+	 * Gets the to
+	 *
+	 * @return string
+	 */
+	public function getTo() {
+		return $this->to;
+	}
+	
+	/**
+	 * Sets the to
+	 *
+	 * @param string $to value to set
+	 * @return this
+	 */
+	public function setTo($to) {
+		$this->to = (string)$to;
+		return $this;
+	}
+	
+	/**
+	 * Gets the from
+	 *
+	 * @return string
+	 */
+	public function getFrom() {
+		return $this->from;
+	}
+	
+	/**
+	 * Sets the from
+	 *
+	 * @param string $from value to set
+	 * @return this
+	 */
+	public function setFrom($from) {
+		$this->from = (string)$from;
+		return $this;
+	}
+	
+	/**
+	 * Gets the route
+	 *
+	 * @return string
+	 */
+	public function getRoute() {
+		return $this->route;
+	}
+	
+	/**
+	 * Sets the route
+	 *
+	 * @param string $route value to set
+	 * @return this
+	 */
+	public function setRoute($route) {
+		$this->route = (string)$route;
+		return $this;
+	}
+	
+	/**
+	 * Gets the ref
+	 *
+	 * @return string
+	 */
+	public function getRef() {
+		return $this->ref;
+	}
+	
+	/**
+	 * Sets the ref
+	 *
+	 * @param string $ref value to set
+	 * @return this
+	 */
+	public function setRef($ref) {
+		$this->ref = (string)$ref;
+		return $this;
+	}
+	
+	/**
+	 * Gets the concat
+	 *
+	 * @return boolean
+	 */
+	public function getConcat() {
+		return $this->concat;
+	}
+	
+	/**
+	 * Sets the concat
+	 *
+	 * @param boolean $concat value to set
+	 * @return this
+	 */
+	public function setConcat($concat) {
+		$this->concat = (boolean)$concat;
+		return $this;
+	}
+	
+	/**
+	 * Gets the senddate
+	 *
+	 * @return integer
+	 */
+	public function getSenddate() {
+		return $this->senddate;
+	}
+	
+	/**
+	 * Sets the senddate
+	 *
+	 * @param integer $senddate value to set
+	 * @return this
+	 */
+	public function setSenddate($senddate) {
+		$this->senddate = (integer)$senddate;
+		return $this;
+	}
+	
+	/**
+	 * Gets the debug
+	 *
+	 * @return boolean
+	 */
+	public function getDebug() {
+		return $this->debug;
+	}
+	
+	/**
+	 * Sets the debug
+	 *
+	 * @param boolean $debug value to set
+	 * @return this
+	 */
+	public function setDebug($debug) {
+		$this->debug = (boolean)$debug;
+		return $this;
+	}
+
+	/**
+	 * sends the sms and returns the status
+	 * 
+	 * @return array
+	 */
+	public function send() {
+		if (
+			!$this->getKey() ||
+			!$this->getMessage() ||
+			!$this->getTo() ||
+			!$this->getFrom() ||
+			!$this->getRoute() ) {
+			return array(
+				'success' => false
+			);
+		}
+
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_URL, self::API_URL);
+		curl_setopt($ch, CURLOPT_HEADER, false);
+		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+		// curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		// disable expect header "100 continue" to prefent HTTP 417 Statuscode
+		curl_setopt($ch, CURLOPT_HTTPHEADER, array('Expect:'));
+		curl_setopt($ch, CURLOPT_POSTFIELDS, array(
+			// required params
+			'key' => $this->getKey(),
+			'message' => $this->getMessage(),
+			'to' => $this->getTo(),
+			'from' => $this->getFrom(),
+			'route' => $this->getRoute(),
+
+			// optional params
+			'debug' => $this->getDebug() ? 1 : 0,
+			'concat' => $this->getConcat() ? 1 : 0,
+			'senddate' => $this->getSenddate(),
+			'ref' => $this->getRef(),
+
+			// output values
+			'message_id' => '1',
+			'cost' => '1',
+			'count' => '1'
+		));
+		$response = explode("\n", curl_exec($ch));
+		curl_close($ch);
+		
+		return array(
+			'success' => $response[0] == 100 ? true : false,
+			'response' => array(
+				'responsecode' => $response[0],
+				'message_id' => $response[1],
+				'cost' => $response[2],
+				'count' => $response[3]
+			)
+		);
+	}
+
+}
+?>
---
+++ README.md
@@ -0,0 +1,60 @@
+## About
+> [Mobilant](http://mobilant.de/) is an ideal platform for sending sms, using this very simple API.
+
+## mobilant class
+The mobilant class is a PHP class for using the [Mobilant](http://mobilant.de/) REST API. Have a look at their API docs for more details about all the parameters.
+
+## minimum configuration
+Fastest possible usage for PHP 5.4+ using chaining for a [fluent interface](http://en.wikipedia.org/wiki/Fluent_interface)
+```php
+(new Mobilant)
+	->setKey('myTokenKey123')
+	->setMessage('hello world')
+	->setTo('0049987123456')
+	->setFrom('Bob')
+	->setRoute('lowcostplus')
+	->send();
+```
+Fastest possible usage for < PHP 5.4 and without using chaining
+```php
+$mobilant = new Mobilant();
+$mobilant->setKey('myTokenKey123');
+$mobilant->setMessage('hello world');
+$mobilant->setTo('0049987123456');
+$mobilant->setFrom('Bob');
+$mobilant->setRoute('lowcostplus');
+$mobilant->send();
+```
+## Methods
+
+Every setter returns the object again to enable chaining and has a respective getter
+
+* setKey
+> Your app API key
+
+* setMessage
+> The message of the sms
+
+* setTo
+> The receiver's phone number
+
+* setFrom
+> The sender name
+
+* setRoute
+> The route to choose
+
+* setRef
+> The reference to save with the message
+
+* setConcat
+> If the message should be concatenated when > 160 chars
+
+* setSenddate
+> The date to send if you want to time it for later
+
+* setDebug
+> If you want to act as if you'd drop a message but don't want it to be sent
+
+* send
+> Send the text message
